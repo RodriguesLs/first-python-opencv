2d5ff7f39964ed4b272193395f0a58140804de67
Rodrigues <fr.costa95@gmail.com>
2019-10-31 09:54:34 -0300

    feat(init): initial commit, karma pattern \n First example with opencv - learn py

---
+++ first-image-analyzing.py
@@ -0,0 +1,19 @@
+#import libraries
+import cv2
+
+#read image
+image = cv2.imread('/home/pi/Pictures/young.jpeg')
+
+print('Width in pixels: ', end='')
+print(image.shape[1])
+print('Height in pixels: ', end='')
+print(image.shape[0])
+print('Quantity channels: ', end='')
+print(image.shape[2])
+
+#Show image with a function imshow
+cv2.imshow('Window name:', image)
+cv2.waitKey(0)
+
+#Save file with another name
+cv2.imwrite('output.jpg', image)
